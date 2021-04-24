107b57252d1df636ce406306f8c405275f6293cc
Yuan Enguang <yuaneg@users.noreply.github.com>
2021-04-24 14:05:49 +0800

    preinstall.sh
    12

---
+++ preinstall.sh
@@ -0,0 +1,20 @@
+#######ttyd 控制台
+opkg install luci-app-ttyd luci-i18n-ttyd-zh-cn
+#######定时自动重启
+opkg install luci-app-autoreboot luci-i18n-autoreboot-zh-cn
+#######cpu性能调节
+opkg install luci-app-cpufreq luci-i18n-cpufreq-zh-cn
+#######helloworld
+opkg install luci-app-vssr luci-i18n-vssr-zh-cn
+#######广告过滤大师
+opkg install luci-app-adbyby-plus luci-i18n-adbyby-plus-zh-cn
+#######京东自动签到
+opkg install luci-app-jd-dailybonus
+#网易云解锁
+opkg install luci-app-unblockmusic luci-i18n-unblockmusic-zh-cn
+#监控界面
+opkg install luci-app-netdata luci-i18n-netdata-zh-cn
+#应用过滤
+opkg install luci-app-appfilter luci-i18n-appfilter-zh-cn
+#tur加速
+opkg install luci-app-turboacc luci-i18n-turboacc-zh-cn	 
